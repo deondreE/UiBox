9bcf6493a5973d7be9c24dbb92af99d75762384e
deondreE <67658523+deondreE@users.noreply.github.com>
2024-01-03 13:40:41 -0600

    initial commit

---
+++ build/UiBox.build/Debug-iphoneos/UiBoxTarget.build/Objects-normal/arm64/UiBoxTarget_vers.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/deondreenglish/Projects/open-source/UiBox/build/Debug-iphoneos/UiBoxTarget.framework/Modules/module.modulemap \
+  /Users/deondreenglish/Projects/open-source/UiBox/build/UiBox.build/Debug-iphoneos/UiBoxTarget.build/DerivedSources/UiBoxTarget_vers.c
---
+++ readme.md
@@ -0,0 +1,10 @@
+# UiBox Component library.
+
+Component library for mobile devices.
+
+# Setup for contributing.
+
+download `xcodegen`.
+
+run `xcodegen generate`
+# UiBox
