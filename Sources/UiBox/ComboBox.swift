1f24da12581baa36948b5cff3ff93fe5f7c6df6f
deondreE <deondre@nil.comp>
2024-01-06 13:56:25 -0600

    combobox update

--- Sources/UiBox/ComboBox.swift
+++ Sources/UiBox/ComboBox.swift
@@ -30,33 +30,36 @@ public struct ComboBox: View {
             }) {
                 Text(selectedOption?.name ?? "Select an option")
                     .foregroundStyle(.black)
-            }
-            
-            if isDropdownVisible {
-
-                VStack {
-                    ForEach(options, id: \.self) { option in
-                        Button(action: {
-                            selectedOption = option
-                            isDropdownVisible.toggle()
-                        }) {
-                            Text(option.name)
+            }.overlay(
+                Group {
+                    if isDropdownVisible {
+                        VStack {
+                            ForEach(options, id: \.self) { option in
+                                Button(action: {
+                                    selectedOption = option
+                                    isDropdownVisible.toggle()
+                                }) {
+                                    Text(option.name)
+                                }
+                                .background(.black)
+                                .foregroundStyle(.white)
+                                .padding(10)
+                            }
                         }
-                        .background(.black)
-                        .foregroundStyle(.white)
-                        .padding(10)
+                        .cornerRadius(8)
+                        .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
+                        .frame(maxWidth: .infinity, maxHeight: 200, alignment: .topLeading)
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color.black)
+                                .stroke(Color.gray, lineWidth: 1)
+                        )
+                        .padding(.horizontal)
                     }
                 }
-                .cornerRadius(8)
-                .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
-                .frame(maxWidth: .infinity, maxHeight: 200, alignment: .topLeading)
-                .background(
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(Color.black)
-                        .stroke(Color.gray, lineWidth: 1)
-                )
-                .padding(.horizontal)
-            }
+                
+            )
+            .frame(maxWidth: .infinity, alignment: .topLeading)
         }
         .onAppear {
             if let jsonData = jsonString.data(using: .utf8) {
