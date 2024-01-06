b07b2856b5c9d1c5193823a6c55ffb3657cc0025
deondreE <deondre@nil.comp>
2024-01-06 16:37:47 -0600

    added more data values, to the project, so that we can test with a larger dataset

--- Sources/UiBox/ComboBox.swift
+++ Sources/UiBox/ComboBox.swift
@@ -140,7 +140,10 @@ public struct ComboBox: View {
     [
         {"id": 1, "name": "Option 1"},
         {"id": 2, "name": "Option 2"},
-        {"id": 3, "name": "Option 3"}
+        {"id": 3, "name": "Option 3"},
+        {"id": 4, "name": "Option 4"},
+        {"id": 5, "name": "Option 5"},
+        {"id": 6, "name": "Option 6"},
     ]
     """
 }
