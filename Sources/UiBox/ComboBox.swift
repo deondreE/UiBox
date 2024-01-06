ddb034610c09cd7d26f04e0c697d831278dcb4e2
deondreE <deondre@nil.comp>
2024-01-06 13:13:04 -0600

    updated combobox to support JSON input

--- Sources/UiBox/ComboBox.swift
+++ Sources/UiBox/ComboBox.swift
@@ -6,11 +6,16 @@
 //
 
 import SwiftUI
+@available(iOS 17.0, macOS 14.0, *)
+struct Option: Identifiable, Codable, Hashable {
+    var id: Int
+    var name: String
+}
 
 @available(iOS 17.0, macOS 14.0, *)
 public struct ComboBox: View {
-    @State public  var selectedOption: String = "Option 1"
-    public  let options = ["Option 1", "Option 2", "Option 2"]
+    @State private var selectedOption: Option?
+    @State private var options: [Option] = []
     
     public init() {
         
@@ -18,14 +23,34 @@ public struct ComboBox: View {
     
     public var body: some View {
         VStack {
-            Text("Selected Option: \(selectedOption)")
+            Text("Selected Option: \(selectedOption?.name ?? "")")
+                .padding()
             
             Picker("Options", selection: $selectedOption) {
-                ForEach(options, id: \.self) {option in
-                    Text(option).tag(option)
+                ForEach(options) {option in
+                    Text(option.name).tag(option)
                 }
             }
+            .pickerStyle(MenuPickerStyle())
             .padding()
+            .onAppear {
+                if let jsonData = jsonString.data(using: .utf8) {
+                    do {
+                        let decodedOptions = try JSONDecoder().decode([Option].self, from: jsonData)
+                        options = decodedOptions
+                    } catch {
+                        print("Error decoding json: \(error)")
+                    }
+                }
+            }
         }
     }
+    // Simulated JSON data
+        let jsonString = """
+        [
+            {"id": 1, "name": "Option 1"},
+            {"id": 2, "name": "Option 2"},
+            {"id": 3, "name": "Option 3"}
+        ]
+        """
 }
