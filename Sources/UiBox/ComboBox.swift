c1cc446b7d7546ddedbd0e8f6e675624c0309ea2
deondreE <deondre@nil.comp>
2024-01-06 15:47:20 -0600

    added height clamp

--- Sources/UiBox/ComboBox.swift
+++ Sources/UiBox/ComboBox.swift
@@ -9,102 +9,104 @@ import SwiftUI
 
 @available(iOS 17.0, macOS 14.0, *)
 struct Option: Identifiable, Codable, Hashable {
-    var id: Int
-    var name: String
+  var id: Int
+  var name: String
 }
 
 @available(iOS 17.0, macOS 14.0, *)
 public struct ComboBox: View {
-    @State private var selectedOption: Option?
-    @State private var options: [Option] = []
-    @State private var filteredOptions: [Option] = []
-    @State private var searchText: String = ""
-    @State private var isDropdownVisible: Bool = false
-    
-    public init() {
-        
-    }
-    
-    public var body: some View {
-        VStack {
-            Button(action: {
-                isDropdownVisible.toggle()
-            }) {
-                Text(selectedOption?.name ?? "Select an option")
-                    .foregroundStyle(.black)
-            }
-            Group {
-                if isDropdownVisible {
-                    VStack {
-                        TextField("Search", text: $searchText)
-                            .padding(.top)
-                            .padding(.horizontal, 2)
-                            .padding(.vertical, 2)
-                            .background(
-                                Rectangle()
-                                    .fill(Color.black)
-                                    .stroke(Color.gray, lineWidth: 1)
-                            )
-                            .foregroundStyle(.white)
-                            .textInputAutocapitalization(.never)
-                            .disableAutocorrection(true)
-                            .onChange(of: searchText) {
-                                if searchText != "" {
-                                    filterOptions()
-                                }
-                            }
-                            
-                        
-                        ForEach(options, id: \.self) { option in
-                            Button(action: {
-                                selectedOption = option
-                                isDropdownVisible.toggle()
-                            }) {
-                                Text(option.name)
-                            }
-                            .background(.black)
-                            .foregroundStyle(.white)
-                            .padding(10)
-                            }
-                        }
-                        .cornerRadius(8)
-                        .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
-                        .frame(maxWidth: 250, maxHeight: 165, alignment: .topLeading)
-                        .background(
-                            RoundedRectangle(cornerRadius: 10)
-                                .fill(Color.black)
-                                .stroke(Color.gray, lineWidth: 1)
-                        )
-                        .padding(.horizontal)
+  @State private var selectedOption: Option?
+  @State private var options: [Option] = []
+  @State private var filteredOptions: [Option] = []
+  @State private var searchText: String = ""
+  @State private var isDropdownVisible: Bool = false
+  @State private var contentHeight: CGFloat = 0
+
+  public init() {
+
+  }
+
+  public var body: some View {
+    VStack {
+      Button(action: {
+        isDropdownVisible.toggle()
+      }) {
+        Text(selectedOption?.name ?? "Select an option")
+          .foregroundStyle(.black)
+      }
+      Group {
+        if isDropdownVisible {
+          GeometryReader { geometry in
+            VStack {
+              TextField("Search", text: $searchText)
+                .padding(.top)
+                .padding(.horizontal, 2)
+                .padding(.vertical, 2)
+                .background(
+                  Rectangle()
+                    .fill(Color.black)
+                    .stroke(Color.gray, lineWidth: 1)
+                )
+                .foregroundStyle(.white)
+                .textInputAutocapitalization(.never)
+                .disableAutocorrection(true)
+                .onChange(of: searchText) {
+                  if searchText != "" {
+                    filterOptions()
+                  }
                 }
-            }
-            .frame(maxWidth: 250, alignment: .topLeading)
-        }
-        .onAppear {
-            if let jsonData = jsonString.data(using: .utf8) {
-                do {
-                    let decodedOptions = try JSONDecoder().decode([Option].self, from: jsonData)
-                    options = decodedOptions
-                } catch {
-                    print("Error decoding json: \(error)")
+
+              ForEach(options, id: \.self) { option in
+                Button(action: {
+                  selectedOption = option
+                  isDropdownVisible.toggle()
+                }) {
+                  Text(option.name)
                 }
+                .background(.black)
+                .foregroundStyle(.white)
+                .padding(10)
+              }
             }
+            .cornerRadius(8)
+            .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
+            .frame(maxWidth: 250, maxHeight: 165, alignment: .topLeading)
+            .frame(height: contentHeight > geometry.size.height ? geometry.size.height : contentHeight)
+            .background(
+              RoundedRectangle(cornerRadius: 10)
+                .fill(Color.black)
+                .stroke(Color.gray, lineWidth: 1)
+            )
+            .padding(.horizontal)
+          }
         }
-    }
-    
-    private func filterOptions( ) {
-        if searchText.isEmpty {
-            filteredOptions = options
-        } else {
-            filteredOptions = options.filter { $0.name.lowercased().contains(searchText.lowercased())}
+      }
+      .onAppear {
+        if let jsonData = jsonString.data(using: .utf8) {
+          do {
+            let decodedOptions = try JSONDecoder().decode([Option].self, from: jsonData)
+            options = decodedOptions
+          } catch {
+            print("Error decoding json: \(error)")
+          }
         }
+      }
     }
-    // Simulated JSON data
-        let jsonString = """
-        [
-            {"id": 1, "name": "Option 1"},
-            {"id": 2, "name": "Option 2"},
-            {"id": 3, "name": "Option 3"}
-        ]
-        """
+  }
+
+  private func filterOptions() {
+    if searchText.isEmpty {
+      filteredOptions = options
+    } else {
+      filteredOptions = options.filter { $0.name.lowercased().contains(searchText.lowercased()) }
+    }
+  }
+  // Simulated JSON data
+  let jsonString = """
+    [
+        {"id": 1, "name": "Option 1"},
+        {"id": 2, "name": "Option 2"},
+        {"id": 3, "name": "Option 3"}
+    ]
+    """
 }
