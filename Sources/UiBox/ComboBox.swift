14d3446efa6eb462c75c4359017f6a8906478251
deondreE <deondre@nil.comp>
2024-01-06 13:57:49 -0600

    remove overaly:

--- Sources/UiBox/ComboBox.swift
+++ Sources/UiBox/ComboBox.swift
@@ -30,35 +30,33 @@ public struct ComboBox: View {
             }) {
                 Text(selectedOption?.name ?? "Select an option")
                     .foregroundStyle(.black)
-            }.overlay(
-                Group {
-                    if isDropdownVisible {
-                        VStack {
-                            ForEach(options, id: \.self) { option in
-                                Button(action: {
-                                    selectedOption = option
-                                    isDropdownVisible.toggle()
-                                }) {
-                                    Text(option.name)
-                                }
-                                .background(.black)
-                                .foregroundStyle(.white)
-                                .padding(10)
+            }
+            Group {
+                if isDropdownVisible {
+                    VStack {
+                        ForEach(options, id: \.self) { option in
+                            Button(action: {
+                                selectedOption = option
+                                isDropdownVisible.toggle()
+                            }) {
+                                Text(option.name)
                             }
+                            .background(.black)
+                            .foregroundStyle(.white)
+                            .padding(10)
                         }
-                        .cornerRadius(8)
-                        .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
-                        .frame(maxWidth: .infinity, maxHeight: 200, alignment: .topLeading)
-                        .background(
-                            RoundedRectangle(cornerRadius: 10)
-                                .fill(Color.black)
-                                .stroke(Color.gray, lineWidth: 1)
-                        )
-                        .padding(.horizontal)
                     }
+                    .cornerRadius(8)
+                    .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
+                    .frame(maxWidth: .infinity, maxHeight: 200, alignment: .topLeading)
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.black)
+                            .stroke(Color.gray, lineWidth: 1)
+                    )
+                    .padding(.horizontal)
                 }
-                
-            )
+            }
             .frame(maxWidth: .infinity, alignment: .topLeading)
         }
         .onAppear {
